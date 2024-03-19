9e6027fe3dd9dbf05016cf7b031455a18ae89c45
JaimeOnaindia <jaimeonaindiaproyectos@gmail.com>
2024-03-19 19:39:40 +0100

    Refactor

--- src/components/DeleteDutyForm.tsx
+++ src/components/DeleteDutyForm.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Form, Input, Button, Modal } from 'antd';
+import { Form, Input, Button } from 'antd';
 import axios from 'axios';
 
 interface DeleteDutyFormProps {
